0628222b4e151e2c86fd47b82598909708efd528
Bhone Wai <demanaosan@gmail.com>
2025-08-03 22:03:45 +0700

    feat: add Custom Avatar

---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,9 @@
+import {Avatar as AntdAvatar} from "antd";
+
+export function CustomAvatar() {
+    return (
+        <AntdAvatar>
+            CP
+        </AntdAvatar>
+    );
+}
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,7 @@
+import {ThemedLayoutV2} from "@refinedev/antd";
+
+export default function Layout() {
+    return (
+        ≤ThemedLayoutV2></ThemedLayoutV2>
+    )
+}
