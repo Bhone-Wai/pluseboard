a5d72141c765e7b4e054f0282b4eaaf028a89f11
Bhone Wai <damenaosan@gmail.com>
2025-08-04 18:00:20 +0700

    feat(component): style application header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,23 @@
 import {CurrentUser} from "./current-user";
+import {Layout} from "antd";
+import React from "react";
 
 export function Header() {
+    const headerStyles: React.CSSProperties = {
+        backgroundColor: '#fff',
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        padding: "0px 24px",
+        height: "64px",
+        position: "sticky",
+        top: 0,
+        zIndex: 999,
+    }
+
     return (
-        <div>
+        <Layout.Header style={headerStyles}>
             <CurrentUser />
-        </div>
-    )
+        </Layout.Header>
+    );
 }
