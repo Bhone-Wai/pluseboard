6a57b7fb501cf46f0f8b7301704b9e88d85be7d5
Bhone Wai <damenaosan@gmail.com>
2025-08-04 18:06:58 +0700

    feat(component): modify custom avatar component

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,9 +1,25 @@
-import {Avatar as AntdAvatar} from "antd";
+import {Avatar as AntdAvatar, AvatarProps} from "antd"
+import {getNameInitials} from "@/utilities/get-name-initials";
 
-export function CustomAvatar() {
+type Props = AvatarProps & {
+    name?: string;
+}
+
+export function CustomAvatar({ name, style, ...rest }: Props) {
     return (
-        <AntdAvatar>
-            CP
+        <AntdAvatar
+            alt={name}
+            size={"small"}
+            style={{
+                backgroundColor: '#87d068',
+                display: "flex",
+                alignItems: "center",
+                border: "none",
+                ...style
+            }}
+            {...rest}
+        >
+            {getNameInitials(name || '')}
         </AntdAvatar>
     );
 }
