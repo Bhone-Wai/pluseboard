c4e7825a7ef7199d8fa39895e11eead3ca53857c
Bhone Wai <damenaosan@gmail.com>
2025-08-06 22:57:27 +0700

    feat(components): add SelectOptionWithAvatar component
    This component will be used to display a user's avatar and name in a select dropdown.

---
+++ src/components/select-option-with-avatar.tsx
@@ -0,0 +1,27 @@
+import {CustomAvatar} from "@/components/custom-avatar";
+import {Text} from "@/components/text";
+
+type Props = {
+    name: string;
+    avatarUrl?: string;
+    shape?: 'circle' | 'square';
+}
+
+export function SelectOptionWithAvatar({ name, avatarUrl, shape }: Props) {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '8px',
+            }}
+        >
+            <CustomAvatar
+                shape={shape}
+                name={name}
+                src={avatarUrl}
+            />
+            <Text>{name}</Text>
+        </div>
+    )
+}
